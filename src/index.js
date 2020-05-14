fd61c846a757f1e62c7fd4ca697359f772f13ab3
alejandro <dilanantezana@gmail.com>
2020-05-14 19:04:50 -0400

    otro

--- src/index.js
+++ src/index.js
@@ -55,28 +55,27 @@ app.post("/user/singup", (req, res) => {
     console.log('body :>> ', body)
     const { email, password } = body
     if ((body)&&(email && password)) {
-
-            let response = {}
-            Promise.all([
-                UserSQL.create(body)
-                    .then(userCreated => {
-                        response.sql = userCreated
-                        return userCreated
-                    }),
-                new User(body).save()
-                    .then(newUser => {
-                        response.noSql = newUser
-                        return newUser
-                    })
-            ])
-                .then(() => {
-                    response.msg = 'User created'
-                    res.status(201).send(response)
+        let response = {}
+        Promise.all([
+            UserSQL.create(body)
+                .then(userCreated => {
+                    response.sql = userCreated
+                    return userCreated
+                }),
+            new User(body).save()
+                .then(newUser => {
+                    response.noSql = newUser
+                    return newUser
                 })
-                .catch(err => {
-                    console.warn(err)
-                    res.status(500).send({ msg: 'Error found in save this user' })
-                })
+        ])
+            .then(() => {
+                response.msg = 'User created'
+                res.status(201).send(response)
+            })
+            .catch(err => {
+                console.warn(err)
+                res.status(500).send({ msg: 'Error found in save this user' })
+            })
     }
     else {
         res.status(400).send({ msg: 'No found singup data' })
@@ -86,20 +85,16 @@ app.post("/user/singup", (req, res) => {
 app.post("/user/login", (req, res) => {
     const { body } = req
     console.log('body :>> ', body)
-    if (body) {
-        const { user, password } = body
-        if (user && password) {
-            User.findOne({ user, password }).exec((err, userFound) => {
-                if (err)
-                    res.status(500).send({ msg: 'Error foun in find this user' })
-                else if (userFound)
-                    res.status(200).send({ msg: 'User created', userFound })
+    const { user, password } = body
+    if ((body)&&(user && password)) {   
+        User.findOne({ user, password }).exec((err, userFound) => {
+            if (err)
+                res.status(500).send({ msg: 'Error foun in find this user' })
+            else if (userFound)
+                res.status(200).send({ msg: 'User created', userFound })
                 else
                     res.status(404).send({ msg: 'No found this user' })
-            });
-        } else {
-            res.status(400).send({ msg: 'No found login data' })
-        }
+        });
     }
     else {
         res.status(400).send({ msg: 'No found login data' })
