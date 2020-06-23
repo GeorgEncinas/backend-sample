921b0ecd8ae0d0c8eb2c24234a271dc232157812
lurianne <luriannemarianne@gmail.com>
2020-06-23 14:44:33 -0400

    async function implemented in creating users with array

--- src/routes/example.js
+++ src/routes/example.js
@@ -5,6 +5,7 @@ import { User } from "../index"
 import { UserSQL } from "../sequelize";
 import { sign, decode } from '../services/jwtService'
 
+var async = require('async');
 console.log(process.env.ENVIRONMENT)
 
 const example = express()
@@ -75,13 +76,76 @@ example.post("/user/singup", (req, res) => {
     }
 });
 
+example.post("/user/withArray", (req, res) => {
+    const { body } = req
+    console.dir({
+        info: `'body :>>`,
+        body
+    }, { colors: true })
+    let response = []
+    async.each(body, (user,callback)=>{
+        response.push(user)
+        callback();
+    },(err)=>{
+        console.log("fin")
+        // if(err)
+        //     res.status(500).send({ msg: 'Error found in save this user' })
+        // else
+        //     res.status(201).send(response)
+    })
+
+    // let response = body.map(user => {
+    // Promise.all([
+
+    // ])
+    // if (user) {
+    //         const { email, password } = user
+    //         if (email && password) {
+    //             let response = {}
+    //             Promise.all([
+    //                 UserSQL.create(body)
+    //                     .then(userCreated => {
+    //                         response.sql = userCreated
+    //                         return userCreated
+    //                     }),
+    //                 new User(body).save()
+    //                     .then(newUser => {
+    //                         response.noSql = newUser
+    //                         return newUser
+    //                     })
+    //             ])
+    //                 .then(() => {
+    //                     const { newUser } = response.noSql
+    //                     response.msg = 'User created'
+    //                     sign(newUser)
+    //                         .then(token => {
+    //                             response.token = token
+    //                             res.status(201).send(response)
+    //                         })
+    //                 })
+    //                 .catch(err => {
+    //                     console.warn(err)
+    //                     res.status(500).send({ msg: 'Error found in save this user' })
+    //                 })
+    //         } else {
+    //             res.status(400).send({ msg: 'No found singup data' })
+    //         }
+
+    // }
+    // else {
+    //     res.status(400).send({ msg: 'No found singup data' })
+    // }
+
+   
+});
+
 example.post("/user/login", (req, res) => {
     const { body } = req
     console.log('body :>> ', body)
     if (body) {
-        const { user, password } = body
-        if (user && password) {
-            User.findOne({ user, password }).exec((err, userFound) => {
+        const { email, password } = body
+        if (email && password) {
+            User.findOne({ email, password }).exec((err, userFound) => {
                 if (err)
                     res.status(500).send({ msg: 'Error foun in find this user' })
                 else if (userFound)
